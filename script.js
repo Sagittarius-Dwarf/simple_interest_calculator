aa19b01c138efd4b711b4bc155394a9b70cbded6
Sagittarius-Dwarf <70897003+Sagittarius-Dwarf@users.noreply.github.com>
2020-09-17 19:25:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
 
     var d = new Date();
-    var newYear = d.getFullYear() + years;
+    var currentYear = d.getFullYear();
+    var newYear = parseInt(currentYear) + parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",<br>at an interest rate of " + rate + ".<br>You will receive an amount of " + interest + ",<br>in the year " + newYear + ".";
 }
-        
+
+function slideVal() {
+    var val = document.getElementById("rate").value;
+    document.getElementById("current").innerHTML = val;
+}
