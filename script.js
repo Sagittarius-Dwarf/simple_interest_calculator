edb30c9780aebbca293216e3e4e6290cbfcc0f64
Sagittarius-Dwarf <70897003+Sagittarius-Dwarf@users.noreply.github.com>
2020-09-22 20:48:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+
 function compute() {
     var principal = document.getElementById("principal").value;
+    
+        if(principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        document.getElementById("result").innerHTML = " ";
+    }
+    
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
